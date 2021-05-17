a4238609b237fd8552f9d00f64dfed3bd508da8e
jiyoon0605 <ljy014@naver.com>
2021-05-17 11:19:13 +0900

    [ADD] triangle

---
+++ triangle/main.js
@@ -0,0 +1,89 @@
+const vertexShaderText = document.querySelector("#vertexShader").text;
+const fragmentShaderText = document.querySelector("#fragmentShader").text;
+
+const initDemo = () => {
+  const canvas = document.querySelector("canvas");
+  const gl = canvas.getContext("webgl");
+
+  if (!gl) {
+    console.log("WebGL not supported, falling back on experimental-webgl");
+    gl = canvas.getContext("experimental-webgl");
+  }
+
+  if (!gl) {
+    alert("Your browser does not support WebGL");
+  }
+
+  gl.clearColor(0.75, 0.85, 0.8, 1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+
+  gl.shaderSource(vertexShader, vertexShaderText);
+  gl.shaderSource(fragmentShader, fragmentShaderText);
+
+  gl.compileShader(vertexShader);
+  gl.compileShader(fragmentShader);
+  if (
+    !gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS) ||
+    !gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)
+  ) {
+    console.log("error");
+    return;
+  }
+
+  const program = gl.createProgram();
+
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.validateProgram(program);
+  if (
+    !gl.getProgramParameter(program, gl.LINK_STATUS) ||
+    !gl.getProgramParameter(program, gl.VALIDATE_STATUS)
+  ) {
+    console.log("error");
+    return;
+  }
+
+  const triangleVertices = [
+    0.0, 0.5, 1.0, 1.0, 0.0, -0.5, -0.5, 0.7, 0.0, 1.0, 0.5, -0.5, 0.1, 1.0,
+    0.6,
+  ];
+
+  const triangleVerticesBufferObject = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, triangleVerticesBufferObject);
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(triangleVertices),
+    gl.STATIC_DRAW
+  );
+
+  const positionAttribLocation = gl.getAttribLocation(program, "vertPosition");
+  const colorAttribLocation = gl.getAttribLocation(program, "vertColor");
+  gl.vertexAttribPointer(
+    positionAttribLocation,
+    2,
+    gl.FLOAT,
+    gl.RENDERBUFFER_ALPHA_SIZE,
+    5 * Float32Array.BYTES_PER_ELEMENT,
+    0
+  );
+  gl.vertexAttribPointer(
+    colorAttribLocation, // Attribute location
+    3, // Number of elements per attribute
+    gl.FLOAT, // Type of elements
+    gl.FALSE,
+    5 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+    2 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+  );
+
+  gl.enableVertexAttribArray(positionAttribLocation);
+  gl.enableVertexAttribArray(colorAttribLocation);
+
+  gl.useProgram(program);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+};
+
+initDemo();
