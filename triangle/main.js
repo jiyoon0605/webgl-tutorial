08426594b7030a3581646e6264cc401836496c26
jiyoon0605 <ljy014@naver.com>
2021-05-24 15:17:39 +0900

    delete

--- triangle/main.js
+++ triangle/main.js
@@ -48,8 +48,27 @@ const initDemo = () => {
   }
 
   const triangleVertices = [
-    0.0, 0.5, 1.0, 1.0, 0.0, -0.5, -0.5, 0.7, 0.0, 1.0, 0.5, -0.5, 0.1, 1.0,
+    0.0,
+    0.5,
+    //
+    1.0,
+    1.0,
+    0.0, //
+
+    -0.5,
+    -0.5,
+    //
+    0.7,
+    0.0,
+    1.0, //
+
+    0.5,
+    -0.5,
+    //
+    0.1,
+    1.5,
     0.6,
+    //
   ];
 
   const triangleVerticesBufferObject = gl.createBuffer();
